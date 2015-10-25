4cfe8c356a1bd9ad96003d2eaa722800bae6c96e
djangokillen <django.lorentzson@hotmail.se>
2015-10-25 20:34:57 +0100

    Move minion answers to own file

--- src/main.py
+++ src/main.py
@@ -13,16 +13,9 @@ def main():
     while True:
         minion_tweets = api.search(q="minions")
 
-        minion_answers = [
-            'STOP TALKING ABOUT MINIONS',
-            'NO MORE MINIONS PLEASE',
-            'OMFG STOP TALKING ABOUT MINIONS',
-            'LIKE LITERALLY WHY DO YOU HAVE TO TWEET ABOUT MINIONS??? THERE\'S NO POINT',
-            'Can you please stop talking about minions? Thanks.',
-            'UGGGH!! STOP. TALKING. ABOUT. MINIONS.',
-            'HOW HARD IS IT? STOP TALKING ABOUT MINIONS!!',
-            'Hey, it\'d be nice if you could stop talking about minions bc they are the WORST'
-        ]
+        answers_file = open('answers/answers.txt', 'r')
+        minion_answers = answers_file.read().splitlines()
+        answers_file.close()
 
         for minion_tweet in minion_tweets:
             sn = minion_tweet.user.screen_name
